02c6f9dde160f2bd01c27f5763a8c703171bcfb9
Hillary Tanui <hillarytanui80@gmail.com>
2025-10-30 21:05:55 +0300

    added  navbar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function NavBar({ cartCount }) {
+  return (
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: "#1e293b",
+        color: "white",
+        padding: "1rem 2rem",
+      }}
+    >
+      <h2>Mini Cart</h2>
+      <div style={{ fontSize: "1.2rem" }}>
+        Cart Items: <strong>{cartCount}</strong>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
